18955c3c31486bb42154d1ad6166cab1f1dda0cd
Aaron <aaronreizork1996@gmail.com>
2025-04-21 09:55:36 -0400

    Logica de Palindromo Feita - em teste

--- bin/leetcode/02. Palindromo.dart
+++ bin/leetcode/02. Palindromo.dart
@@ -1,17 +1,26 @@
 class Solution {
   bool isPalindrome(int x) {
+    // Transformamos o valor e x em uma String
     String deIntParaTexto = x.toString();
+    // Separamos os carateres da string
     List<String> separada = deIntParaTexto.split('');
+    // Damos um reverse, para ordenar de tras para frente
     var ordenar = separada.reversed;
-    print(ordenar.runtimeType);
+    // Transformamos ela novamente em uma String, e juntamos a cadea de texto
     String agoraParaString = ordenar.join().toString();
+    // Agora a trasnformamos em uma int
     int agoraInt = int.parse(agoraParaString);
-
-    return true;
+    // no if, verificamos se o valor inicial de X, e igual ao valor que foi reversado e reordenado
+    // caso seja igual retorna true, se não, retorna false
+    if (x == agoraInt) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
 void main() {
   Solution objeto = Solution();
-  objeto.isPalindrome(10);
+  print(objeto.isPalindrome(121));
 }
