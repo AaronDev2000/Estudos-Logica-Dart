980f531c07c60aa250bf0576e5e5e12845f7076c
Aaron <aaronreizork1996@gmail.com>
2025-04-30 22:35:57 -0400

    ignorando as variaveis não usadas

--- bin/Listas/05. listas_nullable.dart
+++ bin/Listas/05. listas_nullable.dart
@@ -1,12 +1,15 @@
 void main() {
   // Uma lista declarada desta forma não pode receber valores nulos.
+  // ignore: unused_local_variable
   List<String> lista = ['tomas', 'jefferson'];
   // Para que a lista possa aceitar valores nulos, é necessário adicionar o '?' depois do tipo da variável.
+  // ignore: unused_local_variable
   List<String?> novaLista = [null];
   // Desta forma, a lista poderá receber valores nulos.
 
   // Ao colocar o '?' por fora, é possível declarar a lista sem inicializá-la, o que não permite adicionar itens
   // nela, como, por exemplo, o método .add.
+  // ignore: unused_local_variable
   List<String>? lista2;
 
   // Agora a lista pode não ser inicializada e pode receber valores nulos.
