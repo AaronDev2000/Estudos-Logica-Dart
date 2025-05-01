97210aec8e9a20ec33398116c5ad60fdee8f1f60
Aaron <aaronreizork1996@gmail.com>
2025-05-01 17:39:51 -0400

    Treino de Logico na pasta Logic arquivi 01

---
+++ logic/01. declaracao_de_variaveis.dart
@@ -0,0 +1,16 @@
+/*
+1. Declaração de Variáveis
+• Crie um programa que declare variáveis de diferentes tipos (inteiros, strings, doubles, booleans). Atribua valores a essas variáveis e, 
+  em seguida, imprima seus valores e tipos na tela.
+• Exemplo: Declare uma variável para armazenar o nome de uma pessoa, sua idade, altura e se ela está matriculada em um curso.
+*/
+
+void main() {
+  String nome = 'Aaron';
+  int idade = 25;
+  double altura = 1.73;
+  bool matriculado = true;
+  print(
+    'Meu nome é: $nome, tenho $idade anos e $altura de altura. Matriculado em faculdade: $matriculado',
+  );
+}
