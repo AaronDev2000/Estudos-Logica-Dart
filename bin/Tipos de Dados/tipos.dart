c4a08168d519bfbdb46cc3e50beb551f4601480c
Aaron <aaronreizork1996@gmail.com>
2025-05-01 10:47:20 -0400

    Falando de Tipos de Dados

---
+++ bin/Tipos de Dados/tipos.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  // String => 'texto entre aspas, simples ou duplas'
+  String string = 'texto';
+  // int => números inteiros
+  int numero = 25;
+  // double => números decimais
+  double decimal = 4.58;
+  // num => pode armazenar tanto int quanto double
+  num numeroQualquer = 25.45;
+  // bool => verdadeiro ou falso
+  bool verdadeiroOuFalso = true;
+  // dynamic => pode receber qualquer valor
+  dynamic qualquer = 'hola';
+  qualquer = 25;
+  // var => a variável deste tipo irá receber o tipo do valor atribuído a ela, ou seja, o valor irá definir o tipo do objeto
+  var casa = 'aWQE';
+
+  // Object => esse é o tipo pai de todos os outros, então, todos os anteriores são do tipo Object
+}
